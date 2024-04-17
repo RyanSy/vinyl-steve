b94999fc8dde05d2c62accd059f866cb7d5984f2
Ryan Sy <ryansy@HP400.mynetworksettings.com>
2024-04-16 22:10:36 -0400

    connected RSM db; edited shows model; added record riots query

---
+++ models/show.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const showSchema = new Schema(
+  {
+    date: {type: String, required: true},
+    future_dates: [{type: String}],
+    country: {type: String, required: true, max: 75},
+    isInternational: {type: Boolean},
+    name: {type: String, required: true, max: 50},
+    venue: {type: String, required: true, max: 50},
+    address: {type: String, max: 100},
+    address2: {type: String, max: 100},
+    international_address: {type: String, max: 100},
+    city: {type: String, max: 75},
+    state: {type: String, max: 2},
+    zip: {type: String, max: 10},
+    start: {type: String, required: true, max: 50},
+    end: {type: String, required: true, max: 50},
+    date_start: {type: Date, required: true},
+    currency: {type: String, max: 75},
+    regular_admission_fee: {type: Number},
+    early_admission: {type: Boolean},
+    early_admission_fee: {type: Number},
+    early_admission_time: {type: String},
+    number_of_dealers: {type: String},
+    number_of_tables: {type: String},
+    size_of_tables: {type: String, max: 50},
+    table_rent: {type: Number},
+    featured_dealers: [{type: String}],
+    cd_dealers: {type: Boolean},
+    fortyfive_dealers: {type: Boolean},
+    memorabilia_dealers: {type: Boolean},
+    food_drink: {type: Boolean},
+    handicapped_access: {type: Boolean},
+    more_information: {type: String, max: 100},
+    contact_name: {type: String, max: 100},
+    contact_phone: {type: String, max: 100},
+    contact_email: {type: String, max: 100},
+    website: {type: String, max: 100},
+    facebook: {type: String, max: 100},
+    image: {type: String, max: 100},
+    image_public_id: {type: String, max: 100},
+    message: {type: String, max: 100},
+    posted_by: {type: String, required: true, max: 100},
+    date_posted: {type: Date},
+    dealer_rsvp_list: [{type: String}]
+  }, { strict: false }
+);
+
+module.exports = mongoose.model('Show', showSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,95 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 const { requiresAuth } = require('express-openid-connect');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => console.log('MongoDB Connected!'));
+const Show = require('../models/show');
+const moment = require('moment');
+const todaysDate = moment().format('YYYY-MM-DD');
 
-// /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.render('index', { title: 'Express' });
-// });
-
-// req.isAuthenticated is provided from the auth router
+// render index, redirect to home page if logged in
 router.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
+  req.oidc.isAuthenticated() ? res.redirect('/home') : res.render('index', { title: 'Vinyl Steve' });
 });
 
+// render home page with list of record riots
+router.get('/home', async (req, res) => {
+  const user = JSON.stringify(req.oidc.user.name).replace(/"/g, '');
+  const shows = await Show.find({ date: {$gte: todaysDate}, name: /record riot/i });
+  const showsArray = createShowsArray(shows);
+  const showsArraySorted = sortByDateStart(showsArray);
+  const dataObject = {
+    user: user,
+    shows: showsArraySorted
+  };
+  res.render('home', dataObject);
+});
 
+// get user profile info
 router.get('/profile', requiresAuth(), (req, res) => {
   res.send(JSON.stringify(req.oidc.user));
 });
+
+/* helper functions */
+function createShowsArray(shows) {
+  var showsArray = [];
+  for (var i = 0; i < shows.length; i++) {
+    var showObject = {
+      id: shows[i]._id,
+      date: moment(shows[i].date, 'YYYY-MM-DD').format('dddd, MMMM Do, YYYY'),
+      month: moment(shows[i].date, 'YYYY-MM-DD').format('MMM'),
+      day: moment(shows[i].date, 'YYYY-MM-DD').format('D'),
+      year: moment(shows[i].date, 'YYYY-MM-DD').format('YYYY'),
+      date_og: shows[i].date,
+      name: shows[i].name,
+      name_formatted: shows[i].name.toLowerCase().replace(/[\s-@#!$%^&*()_+|~=`{}\[\]:";'<>?,.\/]/g, '-'),
+      country: shows[i].country,
+      isInternational: shows[i].isInternational,
+      international_address: shows[i].international_address,
+      venue: shows[i].venue,
+      address: shows[i].address,
+      city: shows[i].city,
+      state: shows[i].state,
+      zip: shows[i].zip,
+      start: moment(shows[i].start, 'HH:mm').format('LT'),
+      end: moment(shows[i].end, 'HH:mm').format('LT'),
+      date_start: new Date(shows[i].date + ' ' + shows[i].start),
+      currency: shows[i].currency,
+      regular_admission_fee: shows[i].regular_admission_fee,
+      early_admission: shows[i].early_admission,
+      early_admission_time: moment(shows[i].early_admission_time, 'HH:mm').format('LT'),
+      early_admission_fee: shows[i].early_admission_fee,
+      number_of_dealers: shows[i].number_of_dealers,
+      number_of_tables: shows[i].number_of_tables,
+      size_of_tables: shows[i].size_of_tables,
+      table_rent: shows[i].table_rent,
+      featured_dealers: shows[i].featured_dealers,
+      cd_dealers: shows[i].cd_dealers,
+      fortyfive_dealers: shows[i].fortyfive_dealers,
+      memorabilia_dealers: shows[i].memorabilia_dealers,
+      food_drink: shows[i].food_drink,
+      handicapped_access: shows[i].handicapped_access,
+      more_information: shows[i].more_information,
+      contact_name: shows[i].contact_name,
+      contact_phone: shows[i].contact_phone,
+      contact_email: shows[i].contact_email,
+      website: shows[i].website,
+      facebook: shows[i].facebook,
+      image: shows[i].image,
+      image_public_id: shows[i].image_public_id,
+      posted_by: shows[i].posted_by,
+      dealer_rsvp_list: shows[i].dealer_rsvp_list
+    };
+    showsArray.push(showObject);
+  };
+  return showsArray;
+}
+
+function sortByDateStart(showsArray) {
+  return showsArray.sort(function(a, b) {
+    return new Date(a.date_start) - new Date(b.date_start);
+  });
+}
 
 module.exports = router;
