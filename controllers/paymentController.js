4177a5a86844f277dff9cd92a2f7c37c5e1cc9fb
Ryan Sy <ryanbsy@gmail.com>
2025-03-11 22:08:16 -0400

    payment confirmation emails now being sent

--- controllers/paymentController.js
+++ controllers/paymentController.js
@@ -2,6 +2,17 @@ const Show = require('../models/show');
 const Dealer = require('../models/dealer');
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 const mongoose = require('mongoose');
+const nodemailer = require('nodemailer');
+const transporter = nodemailer.createTransport({
+    host: process.env.BREVO_SMTP_SERVER,
+    port: process.env.BREVO_SMTP_PORT,
+    secure: false, // true for port 465, false for other ports
+    auth: {
+        user: process.env.BREVO_LOGIN,
+        pass: process.env.BREVO_SMTP_KEY,
+    },
+});
+
 
 // create stripe checkout session
 exports.create_checkout_session = async (req, res) => {
@@ -43,39 +54,62 @@ exports.create_checkout_session = async (req, res) => {
 // update payment status in db
 exports.payment_conformation = async (req, res) => {
   const id = req.params.id;
-  const email = req.params.email;
+  const dealerEmail = req.params.email;
   const objectId = new mongoose.Types.ObjectId(id);
+  let showName;
+  let showDate;
+  let dealerName;
+  let rentDue;
+  let totalAmountPaid;
 
+  // update show
   await Show.findOneAndUpdate(
     { _id: objectId },
     { $set: { ['dealer_rsvp_list.$[el].paid']: true } },
     {
-      arrayFilters: [{ 'el.email': email }], 
+      arrayFilters: [{ 'el.email': dealerEmail }], 
       new: true
     }
   )
-    // .then(updatedShow => {
-    //   console.log('updatedShow:', updatedShow);
-    // })
-    .catch((err) => {
-      console.log(err);
-      res.render('error', { userName: req.oidc.user.name, userEmail: req.oidc.user.email });
-    });
-
+  .then(show => {
+    console.log('show updated successfully:', show);
+    showName = show.name;
+    showDate = show.date;
+    const dealerRsvpList = show.dealer_rsvp_list;    
+    const dealer = dealerRsvpList.find(({ email }) => email === dealerEmail);
+    dealerName = dealer.name;
+    rentDue = dealer.rent_due;
+    totalAmountPaid = rentDue + 5;
+  })
+  .catch((err) => {
+    console.log(err);
+    res.render('error', { userName: req.oidc.user.name, userEmail: req.oidc.user.email });
+  });
+  
+  // update dealer
   await Dealer.findOneAndUpdate(
-    { email: email },
+    { email: dealerEmail },
     { $set: { ['shows.$[el].paid']: true } },
     { arrayFilters: [{ 'el.id': id }],
       new: true
     }
   )
-    // .then(updatedDealer => {
-    //   console.log('updatedDealer:', updatedDealer);
-    // })
-    .catch((err) => {
-      console.log(err);
-      res.render('error', { userName: req.oidc.user.name, userEmail: req.oidc.user.email });
-    });
+  // .then(dealer => {
+  //   // console.log('dealer updated successfully:', dealer);
+  // })
+  .catch((err) => {
+    console.log(err);
+    res.render('error', { userName: req.oidc.user.name, userEmail: req.oidc.user.email });
+  });
 
+  // send payment confirmation email to Steve
+  await transporter.sendMail({
+    from: '"Vinyl Steve" <info@vinylsteve.com>', // sender address
+    to: ['recordriots@gmail.com', 'ryanbsy@gmail.com'], // recipient
+    subject: `Table rent payment from ${dealerName} for ${showName} on ${showDate}`, // subject line
+    text: `${dealerName} has paid $${totalAmountPaid} for ${showName} on ${showDate}.`, // plain text body
+    html: `<p>${dealerName} has paid $${totalAmountPaid} ($${rentDue} + $5 fee) for "${showName}" on ${showDate}.<p>`
+  });
+  
   res.render('payment-confirmation');
 }
